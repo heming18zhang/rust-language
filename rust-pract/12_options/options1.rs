4a55a3b6689cd2d1de54d4a20a2303bf24ef8739
heming_zhang <jackzh18@gmail.com>
2026-06-12 10:41:17 -0700

    Add files via upload

---
+++ rust-pract/12_options/options1.rs
@@ -0,0 +1,47 @@
+// This function returns how much ice cream there is left in the fridge.
+// If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
+// someone eats it all, so no ice cream is left (value 0). Return `None` if
+// `hour_of_day` is higher than 23.
+fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
+    // TODO: Complete the function body.
+    let mut ice_creams = None;
+    if hour_of_day < 22 {
+        ice_creams = Some(5);
+    } else if hour_of_day == 22 {
+        ice_creams = Some(0);
+    } else if hour_of_day > 23 {
+        ice_creams = None;
+    }
+    ice_creams
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn raw_value() {
+        // TODO: Fix this test. How do you get the value contained in the
+        // Option?
+        let Some(ice_creams) = maybe_ice_cream(12) else {
+            panic!("Expected Some(5), got None");
+        };
+
+        assert_eq!(ice_creams, 5); // Don't change this line.
+    }
+
+    #[test]
+    fn check_ice_cream() {
+        assert_eq!(maybe_ice_cream(0), Some(5));
+        assert_eq!(maybe_ice_cream(9), Some(5));
+        assert_eq!(maybe_ice_cream(18), Some(5));
+        assert_eq!(maybe_ice_cream(22), Some(0));
+        assert_eq!(maybe_ice_cream(23), Some(0));
+        assert_eq!(maybe_ice_cream(24), None);
+        assert_eq!(maybe_ice_cream(25), None);
+    }
+}
